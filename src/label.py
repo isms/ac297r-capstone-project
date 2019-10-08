e43b3a50d813a8186f1de655458e7f1416aa70b2
Josh Feldman <joshfeldman95@gmail.com>
2019-10-08 01:39:41 -0400

    added labelling functionality

---
+++ src/label.py
@@ -0,0 +1,34 @@
+from src.raster import Raster
+import re
+from os import listdir
+from os.path import isfile, join
+from IPython.display import clear_output
+import pandas as pd
+
+data_dir = '../data/raster_sample/'
+label_path = '../labels/labels.csv'
+
+def load_labels(label_path):
+    df = pd.read_csv(label_path, index_col=0)
+    return set(df.index)
+
+def get_label(f):
+    img = Raster(data_dir, f)
+    img.show()
+    label = input("Enter 1 for driveway, 0 for no driveway, and 2 for don't know:")
+    clear_output()
+    return label
+
+def save_label(r, label, label_path):
+    with open(label_path,'a') as label_data:
+        label_data.write(f"{r}, {label}\n")
+
+def label():
+    raster_names = set([f for f in listdir(data_dir) if re.match('.*\.TIF$', f)])
+
+    already_labelled = load_labels(label_path)
+    to_be_labelled = raster_names - already_labelled
+
+    for r in to_be_labelled:
+        label = get_label(r)
+        save_label(r, label, label_path)
--- src/raster.py
+++ src/raster.py
@@ -1,15 +1,18 @@
 import rasterio
 from rasterio.plot import show
 import matplotlib.pyplot as plt
+from IPython import display
 
 class Raster:
     def __init__(self, data_dir, image_name):
         # read raster
-        self.parcel_name = image
-        self.src = rasterio.open(data_dir + image)
+        self.parcel_name = image_name
+        self.src = rasterio.open(data_dir + image_name)
+
         # extract RGB and transpose into (row, col, band)
         self.arr = self.src.read([1,2,3]).transpose(1,2,0)
 
-    def __str__(self):
-        plt.imshow(self.arr)
-        return self.parcel_name
+    def show(self):
+        fig, ax = plt.subplots(figsize=(7, 7))
+        ax.imshow(self.arr)
+        plt.show()
