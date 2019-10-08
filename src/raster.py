5e089b3918d855cb3aa3fada6f9edb3a2171b623
Josh Feldman <joshfeldman95@gmail.com>
2019-10-07 22:48:27 -0400

    implemented raster

---
+++ src/raster.py
@@ -0,0 +1,15 @@
+import rasterio
+from rasterio.plot import show
+import matplotlib.pyplot as plt
+
+class Raster:
+    def __init__(self, data_dir, image_name):
+        # read raster
+        self.parcel_name = image
+        self.src = rasterio.open(data_dir + image)
+        # extract RGB and transpose into (row, col, band)
+        self.arr = src.read([1,2,3]).transpose(1,2,0)
+
+    def __str__(self):
+        plt.imshow(self.arr)
+        return self.parcel_name 
