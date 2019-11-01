bfadb8c226620610aea81d92c0cb8875723afbe7
lipikaramaswamy <lipika.ramaswamy@gmail.com>
2019-11-01 18:34:26 -0400

    added new generator for 2 input images and labels;'

--- src/generator.py
+++ src/generator.py
@@ -3,8 +3,30 @@ import random
 import pandas as pd
 from tensorflow.keras.utils import Sequence
 import numpy as np
-
 from src.raster import Raster
+
+from keras.preprocessing.image import ImageDataGenerator
+
+### New generator for model with two inputs
+def gen_flow_for_two_inputs(sample, aerial_dir = '../data/training/aerial_images/', gsv_dir ='data/training/street_view_images/', batch_size = 32, gsv_image_dim = (640, 640, 3), aer_image_dim = ((2100, 2100, 4))):
+
+	aerial_gen = ImageDataGenerator(horizontal_flip = True, vertical_flip = True, width_shift_range = 0.1,height_shift_range = 0.1,zoom_range = 0.1,rotation_range = 40)
+	gsv_gen = ImageDataGenerator()
+
+	# generator for aerial images
+	aerial_gen = aerial_gen.flow_from_dataframe(sample, directory = aerial_dir, x_col= 'aerial_filename', y_col='temp_label', target_size=(aer_image_dim[0], aer_image_dim[1]), color_mode='rgba', class_mode='categorical', batch_size=batch_size, shuffle=True, seed=100)
+
+	# generator for gsv images
+	gsv_gen = aerial_gen.flow_from_dataframe(sample, directory = gsv_dir, x_col= 'aerial_filename', y_col='temp_label', target_size=(gsv_image_dim[0], gsv_image_dim[1]), color_mode='rgb',class_mode='categorical',batch_size=batch_size,shuffle=True,seed=100)
+
+	# put both together
+	while True:
+		X1_i = aerial_gen.next()
+		X2_i = gsv_gen.next()
+		#Assert arrays are equal - this was for peace of mind, but slows down training
+		#np.testing.assert_array_equal(X1i[0],X2i[0])
+		yield [X1_i[0], X2_i[1]], X1_i[1]
+
 
 
 class DataGenerator(Sequence):
