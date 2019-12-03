a2375319b5c12a0a34ee97f37335053622b40d99
lipikaramaswamy <lipika.ramaswamy@gmail.com>
2019-12-03 12:10:58 -0500

    added model for only streetview cnn

--- src/models.py
+++ src/models.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras import backend as K
 from tensorflow.keras import optimizers
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
 
 def baseline_cnn(image_dim = (100, 100, 5)):
     """baseline cnn for image classification
@@ -24,26 +27,114 @@ def baseline_cnn(image_dim = (100, 100, 5)):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def combined_cnn(gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)), n_classes = 2, loss_fn = 'binary_crossentropy'):
+def street_view_cnn(pretrained_model, image_dim, optimizer, loss, n_classes=3, activation = 'softmax', metrics=['accuracy']):
+    '''
+    Parameters:
+    - pretrained_model: pretrained model with trainable layers previously defined, make sure top layer is not included
+    - image_dim: input image dimensions
+    - optimizer: predefined optimizer
+    - loss: predefined loss
+    
+    Returns:
+    Compiled model
+    '''
+    
+    # 1. Prepare imput image
+    sv_input_img = keras.layers.Input(shape=image_dim, name='satellite_image_input')
+    
+    # 2. Pass input image through pretrained model - note that this model is set to trainable/non0tr
+    pre_trained_embedding = pretrained_model(sv_input_img)
+    
+    # 3. Flatten image embedding
+    flat_pre_trained_embedding = keras.layers.Flatten()(pre_trained_embedding)
+    
+    # 4. Run through some dense layers with batch normalization and dropout
+    full_embedding = keras.layers.Dense(200)(flat_pre_trained_embedding)
+    full_embedding = keras.layers.BatchNormalization()(full_embedding)
+    full_embedding = keras.layers.LeakyReLU(alpha=0.2)(full_embedding)
+
+    full_embedding = keras.layers.Dropout(rate=0.5)(full_embedding)
+
+    full_embedding = keras.layers.Dense(100)(full_embedding)
+    full_embedding = keras.layers.BatchNormalization()(full_embedding)
+    full_embedding = keras.layers.LeakyReLU(alpha=0.3)(full_embedding)
+
+    full_embedding = keras.layers.Dropout(rate=0.2)(full_embedding)
+
+    full_embedding = keras.layers.Dense(50)(full_embedding)
+    full_embedding = keras.layers.BatchNormalization()(full_embedding)
+    full_embedding = keras.layers.LeakyReLU(alpha=0.3)(full_embedding)
+
+    full_embedding = keras.layers.Dropout(rate=0.2)(full_embedding)
+    
+    # 5. Define output
+    output = keras.layers.Dense(n_classes, activation=activation)(full_embedding)
+    
+    # 6. Define Model 
+    model = keras.models.Model(inputs=sv_input_img, outputs=output)
+    
+    # 7. Compine Model
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+    
+    return model
+
+def satellite_cnn(sat_image_dim = (256, 256, 3),  n_classes = 3, loss_fn = 'categorical_crossentropy', final_layer_activation='softmax', learning_rate = 1e-3):
+    
+    """
+    CNN to classify satellite images
+    3 classes - no driveway (0), driveway (1), unsure (2)
+    """
+    # 1. Satellite Image input
+    sat_input_img = layers.Input(shape=sat_image_dim, name='satellite_image_input')
+    
+    # 2. Define Pretrained CNN - Inception V3
+    pretrained_model = InceptionV3(include_top=False,        # this removes the final dense layer
+                           input_shape=sat_input_img)
+    
+    print('loaded pretrained')
+    # freeze all layers but the top 13 in the model
+    for layer in pretrained_model.layers[:-13]:
+        layer.trainable = False
+    
+    # Get image embedding
+    sat_cnn = pretrained_model(sat_input_img)
+    
+#     sat_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(sat_input_img)
+#     sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
+#     sat_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(sat_cnn)
+#     sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
+#     sat_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(sat_cnn)
+#     sat_flat = layers.Flatten()(sat_cnn)
+    
+    # 3. Add some dense layers
+    sat_image_embedding = layers.Dense(1000, activation='relu')(sat_cnn)
+    sat_image_embedding = layers.Dense(250, activation='relu')(sat_image_embedding)
+    
+    output = layers.Dense(n_classes, activation=final_layer_activation)(sat_image_embedding)
+
+    # 5. define full model and compile
+    model = models.Model(inputs=sat_input_img, outputs=output)
+
+    adam = optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=False)
+
+    model.compile(loss=loss_fn, optimizer=adam, metrics=['accuracy'])
+
+    return model
+
+def combined_cnn(gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)), n_classes = 2, loss_fn = 'binary_crossentropy', final_layer_activation='sigmoid', learning_rate = 1e-3):
 
     """
     Combined CNN using satellite and Street View imagery.
     """
-    # functions from http://www.deepideas.net/unbalanced-classes-machine-learning/
-    def sensitivity(y_true, y_pred):
-        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-        return true_positives / (possible_positives + K.epsilon())
-
-    def specificity(y_true, y_pred):
-        true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
-        possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
-        return true_negatives / (possible_negatives + K.epsilon())
 
     # 1. Google Street View (GSV) Image Input
     gsv_input_img = layers.Input(shape=gsv_image_dim, name='gsv_image_input')
 
-    gsv_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(gsv_input_img)
+    gsv_cnn = layers.Conv2D(256, (3,3), activation = 'relu')(gsv_input_img)
+    gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
+    gsv_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(gsv_cnn)
+    gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
+    gsv_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(gsv_cnn)
     gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
     gsv_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(gsv_cnn)
     gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
@@ -56,9 +147,9 @@ def combined_cnn(gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)
 
     sat_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(sat_input_img)
     sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
-    sat_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(sat_cnn)
+    sat_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(sat_cnn)
     sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
-    sat_cnn = layers.Conv2D(32, (3,3), activation = 'relu')(sat_cnn)
+    sat_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(sat_cnn)
     sat_flat = layers.Flatten()(sat_cnn)
     sat_image_embedding = layers.Dense(200, activation='relu')(sat_flat)
 
@@ -66,20 +157,21 @@ def combined_cnn(gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)
     concat = layers.Concatenate(axis=1)([gsv_image_embedding, sat_image_embedding])
 
     # 4. Dense layers + classification
-    full_embedding = layers.Dense(300, activation='relu')(concat)
+    full_embedding = layers.BatchNormalization()(concat)
     full_embedding = layers.Dense(100, activation='relu')(full_embedding)
-    output = layers.Dense(n_classes, activation='sigmoid')(full_embedding)
+    full_embedding = layers.Dense(50, activation='relu')(full_embedding)
+    output = layers.Dense(n_classes, activation=final_layer_activation)(full_embedding)
 
     # 5. define full model and compile
     model = models.Model(inputs=[gsv_input_img, sat_input_img], outputs=output)
 
-    adam = optimizers.Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    adam = optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=False)
 
-    model.compile(loss=loss_fn, optimizer=adam, metrics=['accuracy', sensitivity, specificity])
+    model.compile(loss=loss_fn, optimizer=adam, metrics=['accuracy'])
 
     return model
 
-def three_input_model(n_tabular_cols = 10, gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)), n_classes = 1, loss_fn = 'binary_crossentropy'):
+def three_input_model(n_tabular_cols = 10, gsv_image_dim = (640, 640, 3), sat_image_dim = ((2100, 2100, 4)), n_classes = 1, loss_fn = 'binary_crossentropy',final_layer_activation='softmax'):
 
     """
     Combined CNN using satellite and Street View imagery.
@@ -99,41 +191,70 @@ def three_input_model(n_tabular_cols = 10, gsv_image_dim = (640, 640, 3), sat_im
     # 1. Google Street View (GSV) Image Input
     gsv_input_img = layers.Input(shape=gsv_image_dim, name='gsv_image_input')
 
-    gsv_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(gsv_input_img)
+    gsv_cnn = layers.Conv2D(256, (3,3))(gsv_input_img)
+    gsv_cnn = layers.BatchNormalization()(gsv_cnn)
+    gsv_cnn = layers.Activation("relu")(gsv_cnn)
+    gsv_cnn = layers.Dropout(0.2)(gsv_cnn)
+    
     gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
-    gsv_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(gsv_cnn)
-    gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
-    gsv_cnn = layers.Conv2D(32, (3,3), activation = 'relu')(gsv_cnn)
-    gsv_flat = layers.Flatten()(gsv_cnn)
-    gsv_image_embedding = layers.Dense(50, activation='relu')(gsv_flat)
+    
+    gsv_cnn = layers.Conv2D(128, (3,3))(gsv_cnn)
+    gsv_cnn = layers.BatchNormalization()(gsv_cnn)
+    gsv_cnn = layers.Activation("relu")(gsv_cnn)
+    gsv_cnn = layers.Dropout(0.2)(gsv_cnn)
 
+    gsv_cnn = layers.MaxPooling2D((2,2))(gsv_cnn)
+    
+    gsv_cnn = layers.Conv2D(64, (3,3))(gsv_cnn)
+    gsv_cnn = layers.BatchNormalization()(gsv_cnn)
+    gsv_cnn = layers.Activation("relu")(gsv_cnn)
+    gsv_cnn = layers.Dropout(0.2)(gsv_cnn)
+
+    gsv_flat = layers.Flatten()(gsv_cnn)
+    gsv_image_embedding = layers.Dense(100, activation='relu')(gsv_flat)
+    
     # 2. Satellite Image input
     sat_input_img = layers.Input(shape=sat_image_dim, name='aerial_image_input')
+    sat_cnn = layers.Dropout(0.2)(sat_input_img)
+    
+    sat_cnn = layers.Conv2D(256, (3,3))(sat_cnn)
+    sat_cnn = layers.BatchNormalization()(sat_cnn)
+    sat_cnn = layers.Activation("relu")(sat_cnn)
+    sat_cnn = layers.Dropout(0.2)(sat_cnn)
+    
+    sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
+    
+    sat_cnn = layers.Conv2D(128, (3,3))(sat_cnn)
+    sat_cnn = layers.BatchNormalization()(sat_cnn)
+    sat_cnn = layers.Activation("relu")(sat_cnn)
+    sat_cnn = layers.Dropout(0.2)(sat_cnn)
 
-    sat_cnn = layers.Conv2D(128, (3,3), activation = 'relu')(sat_input_img)
     sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
-    sat_cnn = layers.Conv2D(64, (3,3), activation = 'relu')(sat_cnn)
-    sat_cnn = layers.MaxPooling2D((2,2))(sat_cnn)
-    sat_cnn = layers.Conv2D(32, (3,3), activation = 'relu')(sat_cnn)
+    
+    sat_cnn = layers.Conv2D(64, (3,3))(sat_cnn)
+    sat_cnn = layers.BatchNormalization()(sat_cnn)
+    sat_cnn = layers.Activation("relu")(sat_cnn)
+    sat_cnn = layers.Dropout(0.2)(sat_cnn)
+
     sat_flat = layers.Flatten()(sat_cnn)
-    sat_image_embedding = layers.Dense(50, activation='relu')(sat_flat)
+    sat_image_embedding = layers.Dense(100, activation='relu')(sat_flat)
 
     # 3. Tabular Data
     tabular_input = layers.Input(shape=(n_tabular_cols,), name='tabular_input')
-    tabular_layers = layers.Dense(100, activation='relu',  name='tabular_dense1')(tabular_input)
-    tabular_embedding = layers.Dense(50, activation='relu')(tabular_input)
+    tabular_layers = layers.Dense(200, activation='relu',  name='tabular_dense1')(tabular_input)
+    tabular_embedding = layers.Dense(100, activation='relu')(tabular_input)
 
     # 4. Concatenate embeddings
     concat = layers.Concatenate(axis=1)([gsv_image_embedding, sat_image_embedding, tabular_embedding])
 
     # 5. Dense layers + classification
-    full_embedding = layers.Dense(300, activation='relu')(concat)
-    full_embedding = layers.Dense(100, activation='relu')(full_embedding)
-    output = layers.Dense(n_classes, activation='sigmoid')(full_embedding)
+    full_embedding = layers.Dense(150, activation='relu')(concat)
+    full_embedding = layers.Dense(50, activation='relu')(full_embedding)
+    output = layers.Dense(n_classes, activation = final_layer_activation)(full_embedding)
 
     # 6. define full model and compile
     model = models.Model(inputs=[gsv_input_img, sat_input_img, tabular_input], outputs=output)
-    adam = optimizers.Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    adam = optimizers.Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, amsgrad=False)
     model.compile(loss=loss_fn, optimizer=adam, metrics=['accuracy', sensitivity, specificity])
 
     return model
